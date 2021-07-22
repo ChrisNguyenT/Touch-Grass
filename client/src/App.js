bd44ed4161ee89aa4faa5d9cae3a2c20a3290aa1
Ian <63839774+Lolikerz@users.noreply.github.com>
2021-07-22 13:47:19 -0400

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-
+import ProtectedRoute from './components/ProtectedRoute';
+import Unauthorized from './components/Unauthorized';
 // import { Nav, Footer, Home, Map, User, Login } from "./components";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Login from "./components/login.component";
 import SignUp from "./components/signup.component";
 
-
-/*function App() {
+/*
+function App() {
 
 
   return (<Router>
@@ -28,28 +29,40 @@ import SignUp from "./components/signup.component";
       </div>
     </div></Router>
   );
+}*/
+
+function App() {
+  const [user, setUser] = useState(false)
+
+  const handleLogin = e => {
+    e.preventDefault();
+    setUser(true);
+  }
+
+  const handleLogout = e => {
+    e.preventDefault();
+    setUser(false);
+  }
+
+  return (
+    <div className="App">
+      <Router>
+      <Nav />
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+        <Route exact path='/' handleLogin={handleLogin} render={
+          props => <Login {...props} user={user.toString()}
+            handleLogin={handleLogin} />} />
+        <ProtectedRoute exact path='/login' user={user} handleLogout={handleLogout} component={Login} />
+        <Route exact path='/unauthorized' component={Unauthorized} />
+        </div>
+    </div>
+      </Router>
+  </div>
+  );
 }
 
-export default App;
-*/
-
-
-const App = () => {
-  return (
-    <div className="center w85">
-      <div className="ph3 pv1 background-gray">
-        <Switch>
-          <Route exact path="/" component={LinkList} />
-          <Route
-            exact
-            path="/create"
-            component={CreateLink}
-          />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </div>
-    </div>
-  );
-};
 
 export default App;
+
+
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
-import { AUTH_TOKEN } from './constants';
-import { setContext } from '@apollo/client/link/context';
 
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
-});
 
 ReactDOM.render(
   <BrowserRouter>
@@ -18,12 +12,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem(AUTH_TOKEN);
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : ''
-    }
-  };
-});
+
