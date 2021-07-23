a8138c817282fd4c72eb80ec896fad94196905a7
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-23 06:41:06 -0400

    completely overhauled server side. See Chetan's main branch for all the commits.

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import ProtectedRoute from './components/ProtectedRoute';
 import Unauthorized from './components/Unauthorized';
+import { Redirect } from 'react-router';
+import { setContext } from '@apollo/client/link/context';
 // import { Nav, Footer, Home, Map, User, Login } from "./components";
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Login from "./components/login.component";
 import SignUp from "./components/signup.component";
+//import Auth from '../src/utils/auth';
 
-/*
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  
+  const token = localStorage.getItem('id_token');
+  return {
+
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
 
 
@@ -29,8 +54,8 @@ function App() {
       </div>
     </div></Router>
   );
-}*/
-
+}
+/*
 function App() {
   const [user, setUser] = useState(false)
 
@@ -61,7 +86,7 @@ function App() {
   </div>
   );
 }
-
+*/
 
 export default App;
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,17 +5,17 @@ function Home() {
     <div className="home" style={{ 
       backgroundImage: `url(${process.env.PUBLIC_URL + '/grass.png'})` 
     }}>
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
+      <div className="container">
+        <div className="row align-items-center my-5">
+          <div className="col-lg-7">
             <img
-              class="img-fluid rounded mb-4 mb-lg-0"
+              className="img-fluid rounded mb-4 mb-lg-0"
               src="http://placehold.it/900x400"
               alt=""
             />
           </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
+          <div className="col-lg-5">
+            <h1 className="font-weight-light">Home</h1>
             <p>
               Welcome to Touch Grass!
             </p>
