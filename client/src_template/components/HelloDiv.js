5a3e7792b67a0afca28b57480e9cf1d6f25766d8
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-23 01:34:19 -0400

    fixed fatal issues front page now renders. lots of work still needs to be done however

---
+++ client/src_template/components/HelloDiv.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TestingDiv() {
+  return (
+    <div>
+      <h1>this is a test!</h1>
+      <ul>
+        <li>this</li>
+        <li>should</li>
+        <li>render!</li>
+      </ul>
+    </div>
+  );
+}
+
+export default HelloDiv;
