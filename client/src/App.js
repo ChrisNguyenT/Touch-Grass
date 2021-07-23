2ac321b45b87924e2ab3adb7c897220a3d4be1aa
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-23 03:48:18 -0400

    edited client code to work with apolloclient

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import ProtectedRoute from './components/ProtectedRoute';
 import Unauthorized from './components/Unauthorized';
+import { Redirect } from 'react-router';
+import { setContext } from '@apollo/client/link/context';
 // import { Nav, Footer, Home, Map, User, Login } from "./components";
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Login from "./components/login.component";
 import SignUp from "./components/signup.component";
+import Auth from '../src/utils/auth';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+})
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
 
